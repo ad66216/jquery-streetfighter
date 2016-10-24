6cec77db5d7924b857f203db672b2f20e7404827
ad66216 <ad66216@gmail.com>
2016-10-24 19:25:58 -0400

    After implementing audio for hadouken throw

--- js/app.js
+++ js/app.js
@@ -9,12 +9,30 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'}, 500,
+			function() {
+				$('.hadouken').hide();
+				$('.hadouken').css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
 	});
+
+});
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+$('body').keydown(function(e) {
+	if (e.keycode == 88) {
+		alert("X was pressed");
+	}
 });
